f6e23ef42219262d41b62e5ccf0f38ce7700baf1
gardenviki-prog <gardenviki@gmail.com>
2026-05-07 23:34:15 +0300

    Add seventh exercise for IntermediateMachineLearning

---
+++ IntermediateMachineLearning/exercise_7_data_leakage.py
@@ -0,0 +1,12 @@
+# Set up code checking
+from learntools.core import binder
+binder.bind(globals())
+from learntools.ml_intermediate.ex7 import *
+print("Setup Complete")
+
+
+# Fill in the line below with one of 1, 2, 3 or 4.
+potential_leakage_feature = 2
+
+# Check your answer
+q_5.check()
